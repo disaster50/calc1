ab5feae7aa05825779384f220e6585ac39a0466d
disaster50 <vishalmalviya50@gmail.com>
2016-08-26 01:14:27 +0530

    Update add.c

--- add.c
+++ add.c
@@ -6,7 +6,8 @@ uint32_t add(int num1, int num2)
 void display(uint32_t result)
 {
 	printf("Result: %lu", result);
-	printf("Patch modified something example");
+	printf("Patch example");
+	printf("4Patch example");
 	printf("2Patch example");
 	printf("3Patch example");
 }
